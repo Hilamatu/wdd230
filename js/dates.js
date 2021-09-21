f0def22aa5c2e0f2b4938eb10e22dfb86998bcae
Hilamatu <hil21006@byui.edu>
2021-09-21 20:54:00 +0900

    portal homepage

--- js/dates.js
+++ js/dates.js
@@ -1,2 +1,6 @@
 const copyrightyear = document.getElementById("copyrightyear");
 copyrightyear.textContent = new Date().getFullYear();
+
+
+const lastmodified = document.getElementById("lastmodified")
+lastmodified.textContent = new Date(document.lastModified);
