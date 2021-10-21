a4830197be17bd3edd87e235ff0b77efbf1f53e6
Hilamatu <hil21006@byui.edu>
2021-10-21 20:31:54 +0900

    windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,10 +1,13 @@
-let temp = parseFloat(document.getElementById("temp"))
-let speed = parseFloat(document.getElementById("speed"))
+let temp = parseInt(document.querySelector('#temp').innerHTML);
+let speed = parseInt(document.querySelector('#speed').innerHTML);
 
-document.getElementById("windChill").innerHTML = windChillCalculator(temp, speed)
-
-function windChillCalculator(temp, speed) {
-    let windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-    windChill = Math.floor(windChill);
-    return windChill
+let windChill;
+if (temp <= 50 && speed >= 3) {
+  windChill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16))).toFixed(0);
 }
+
+else {
+  windChill = "N/A";
+}
+
+document.querySelector('#windChill').textContent = windChill;
