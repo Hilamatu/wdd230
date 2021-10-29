60d78da91408ee7eac0a8af44e13b29a08b51794
Hilamatu <hil21006@byui.edu>
2021-10-30 06:45:55 +0900

    date

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -3,17 +3,18 @@ let today = new Date(Date.now())
 const milisecondsToDays = 86400000;
 
 
-
 const lastVisit = JSON.parse(window.localStorage.getItem('lastvisit'))
 
 let lastVisitDate = new Date(lastVisit)
 
 let numberOfDays;
-if (lastVisit !== 0) {
-    numberOfDays = Math.round((lastVisitDate - today) / milisecondsToDays)
+if (lastVisitDate !== 0) {
+    numberOfDays = Math.round((today - lastVisitDate) / milisecondsToDays)
 }
 else {
     numberOfDays = 0
 }
 
 document.getElementById("days").textContent = numberOfDays
+
+window.localStorage.setItem('lastvisit', JSON.stringify(today))
