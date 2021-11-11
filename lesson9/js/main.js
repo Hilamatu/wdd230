2e8635067bdf24f33460612108c84309dfae1318
Hilamatu <hil21006@byui.edu>
2021-11-11 20:36:46 +0900

    json

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -2,4 +2,9 @@ const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json"
 
 fetch(requestURL)
     .then(function(response) {
-    return  response.json()*}
+    return  response.json()
+    })
+
+    .then(function (jsonObject) {
+        const town = jsonObject ["towns"]
+    })
