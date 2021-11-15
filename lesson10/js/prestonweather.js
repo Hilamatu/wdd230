13566102a93e2eba801ed3b53006be512b01db69
Hilamatu <hil21006@byui.edu>
2021-11-16 05:42:06 +0900

    weather

--- lesson10/js/prestonweather.js
+++ lesson10/js/prestonweather.js
@@ -1,3 +1,4 @@
+// Get the current weather
 const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=39118d2fb9fc18f735a898398fe6dfa3"
 
 fetch(apiURL)
@@ -22,7 +23,7 @@ fetch(apiURL)
             // Declare a variable to calculate and store the windChill
             calculation = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16))).toFixed(0);
             // Declare a variable to concatenate the windchill and °F
-            windChill = calculation + '°F' 
+            windChill = calculation + '°F'
         }
         // If the windChill cannot be calculated, will pass N/A to windChill variable
         else {
@@ -31,4 +32,57 @@ fetch(apiURL)
 
         // Will pass the windChill variable value to windChill ID of HTML document.
         document.querySelector('#windChill').textContent = windChill;
+    });
+
+// Get the forecast for 5 days
+const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=021e6802b9ec4c17d8adb058949ab7c7"
+
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((forecast) => {
+        console.log(forecast)
+
+        const list = forecast.list
+
+        // Declare an empty array to store the returned value from the for loop that will get the five forecasts based on time (18:00:00)
+        let fiveDays = [];
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].dt_txt.includes("18:00:00")) {
+                fiveDays.push(list[i])
+            }
+        }
+
+        // Define a function to convert the unix date to human date format
+        // the parameter will the "dt" from fiveDays list in unix format
+        function unixToDate(dt) {
+            const miliseconds = dt * 1000
+            const dateObject = new Date(miliseconds)
+            const weekDay = dateObject.toLocaleString("default", { weekday: "short" })
+            return weekDay
+        }
+
+        // Set the days of the week
+        document.getElementById("weekday1").textContent = unixToDate(fiveDays[0].dt);
+        document.getElementById("weekday2").textContent = unixToDate(fiveDays[1].dt);
+        document.getElementById("weekday3").textContent = unixToDate(fiveDays[2].dt);
+        document.getElementById("weekday4").textContent = unixToDate(fiveDays[3].dt);
+        document.getElementById("weekday5").textContent = unixToDate(fiveDays[4].dt);
+
+
+        // Set the temperature
+        document.getElementById("data1").textContent = fiveDays[0].main.temp
+        document.getElementById("data2").textContent = fiveDays[1].main.temp
+        document.getElementById("data3").textContent = fiveDays[2].main.temp
+        document.getElementById("data4").textContent = fiveDays[3].main.temp
+        document.getElementById("data5").textContent = fiveDays[4].main.temp
+
+        // Set the weather icon
+        const imageLink = "https://openweathermap.org/img/w/";
+        document.getElementById("img1").setAttribute("src", `${imageLink}${fiveDays[0].weather[0].icon}.png`)
+        document.getElementById("img2").setAttribute("src", `${imageLink}${fiveDays[1].weather[0].icon}.png`)
+        document.getElementById("img3").setAttribute("src", `${imageLink}${fiveDays[2].weather[0].icon}.png`)
+        document.getElementById("img4").setAttribute("src", `${imageLink}${fiveDays[3].weather[0].icon}.png`)
+        document.getElementById("img5").setAttribute("src", `${imageLink}${fiveDays[4].weather[0].icon}.png`)
+    
+
     })
