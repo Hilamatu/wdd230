9f8db09d332b2dcdb2208da8c15e2bb9aba5b577
Hilamatu <hil21006@byui.edu>
2021-11-24 04:27:27 +0900

    blur

--- lesson11/js/gallery.js
+++ lesson11/js/gallery.js
@@ -31,32 +31,33 @@ window.localStorage.setItem('lastvisit', JSON.stringify(today))
 
 const images = document.querySelectorAll("[data-src]");
 
-
 function preloadImage(img) {
     const src = img.getAttribute("data-src");
     if (!src) {
         return;
-    }
+    };
 
     img.src = src;
-}
+    img.onload = () => {
+        img.removeAttribute('data-src');
+    };
+};
 
 const imgOptions = {
-    threshold: 1,
+    threshold: 0,
     rootMargin: "0px 0px 300px 0px"
 };
 
-const imgObserver = new IntersectionObserver((entries,
-    imgObserver) => {
-        entries.forEach(entry => {
-            if(!entry.isIntersecting) {
-                return;
-            } else {
-                preloadImage(entry.target);
-                imgObserver.unobserve(entry.target);
-            }
-        })
-    }, imgOptions);
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target)
+        };
+    });
+}, imgOptions);
 
 images.forEach(image => {
     imgObserver.observe(image);
